5669df19f2c08f2d535fccd0ee00110078c57789
joro <jose.alonsozv12@gmail.com>
2024-10-23 21:40:30 -0500

    Adding Chair 3D Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Headphone } from "./components/Headphone";
+import { Chair } from "./components/Chair";
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
@@ -9,7 +10,7 @@ function App() {
     <Canvas>
       <ambientLight intensity={2} />
       <OrbitControls />
-      <Headphone />
+      <Chair />
     </Canvas>
   );
 }
