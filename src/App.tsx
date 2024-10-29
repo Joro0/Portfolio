9fc0fa4be3cfa5dfd0123a3c4a513ec03061e8e1
joro <jose.alonsozv12@gmail.com>
2024-10-29 00:17:08 -0500

    Added svg image and pattern.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import AboutImage from "./components/AboutImage";
+import AboutPattern from "./components/AboutPattern";
 import AboutText from "./components/AboutText";
 import CanvasModels from "./components/CanvasModels";
 import Header from "./components/Header";
@@ -8,7 +9,7 @@ import IntroText from "./components/IntroText";
 function App() {
   return (
     <>
-      <div id="main-container">
+      <div id="main-container" style={{ position: "relative" }}>
         <Header />
 
         <section id="intro-section">
@@ -18,6 +19,7 @@ function App() {
 
         <section id="about-section">
           <AboutText />
+          <AboutPattern />
           <AboutImage />
         </section>
         <section id="tool-section"></section>
