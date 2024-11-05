d70c996e3e3e5e1216ea5d33f52adce275e08e19
joro <jose.alonsozv12@gmail.com>
2024-11-05 16:15:36 -0500

    Added switch case for tool imgs / Installed and added Lottie animation

--- src/components/ToolDescp.tsx
+++ src/components/ToolDescp.tsx
@@ -1,9 +1,28 @@
 import { OrbitControls } from "@react-three/drei";
 import { ForestHouse } from "./ForestHouse";
 import { Canvas } from "@react-three/fiber";
+import ToolAnimation from "../assets/ToolAnimation.json";
+import Lottie from "lottie-react";
 
 const ToolDescp = () => {
   const canvasSquareSize = 300;
+  const getToolImage = (type: string) => {
+    switch (type) {
+      case "frontend":
+        return <p>front end Test</p>;
+      case "3D":
+        return (
+          <Canvas style={{ width: "100%", height: "100%" }}>
+            <OrbitControls />
+            <ForestHouse scale={1.05} position={[0, -0.1, 0]} />
+          </Canvas>
+        );
+      case "2D":
+        return <Lottie animationData={ToolAnimation} />;
+      default:
+        return <p>default Test</p>;
+    }
+  };
 
   return (
     <div
@@ -22,10 +41,7 @@ const ToolDescp = () => {
           backgroundColor: "white",
         }}
       >
-        <Canvas style={{ width: "100%", height: "100%" }}>
-          <OrbitControls />
-          <ForestHouse scale={1.05} position={[0, -0.1, 0]} />
-        </Canvas>
+        {getToolImage("2D")}
       </div>
     </div>
   );
