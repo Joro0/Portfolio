be540bb19b939a5f180ff5322be542449c71b810
joro <jose.alonsozv12@gmail.com>
2024-11-14 11:41:49 -0500

    Changed Tool section: added img dependency, removed ToolDescp, ToolGrid, enhaced coloring

--- src/components/Tool/Tool.tsx
+++ src/components/Tool/Tool.tsx
@@ -1,17 +1,81 @@
 import "./Tool.scss";
-import ToolDescp from "./ToolDescp";
-import ToolGrid from "./ToolGrid";
+import ToolSvg from "./ToolSvg";
+import { useState } from "react";
+import ToolRefImg from "./ToolRefImg";
 
 interface Props {
   id: string;
 }
 
 const Tool = ({ id }: Props) => {
+  const frontEndList = [
+    "javascript",
+    "typescript",
+    "react",
+    "angular",
+    "next",
+    "git",
+    "redux",
+    "sass",
+  ];
+  const threeDList = ["three", "fusion", "blender"];
+  const twoDList = ["figma", "illustrator"];
+  const titleList = ["FRONT-END", "3D", "2D"];
+
+  const [clickedZone, setClickedZone] = useState<number>(0);
   return (
     <section id={id}>
       <div className="tool-pattern"></div>
-      <ToolGrid />
-      <ToolDescp />
+
+      <div className="tool-grid">
+        {frontEndList.map((icon, index) => (
+          <div
+            className={`icon-${index + 1} ${
+              clickedZone === 1 ? "grid-selected" : ""
+            }`}
+            onClick={() => {
+              setClickedZone(1);
+            }}
+          >
+            <ToolSvg svg={icon} />
+          </div>
+        ))}
+        {threeDList.map((icon, index) => (
+          <div
+            className={`icon-${index + 9} ${
+              clickedZone === 2 ? "grid-selected" : ""
+            }`}
+            onClick={() => {
+              setClickedZone(2);
+            }}
+          >
+            <ToolSvg svg={icon} />
+          </div>
+        ))}
+        {twoDList.map((icon, index) => (
+          <div
+            className={`icon-${index + 12} ${
+              clickedZone === 3 ? "grid-selected" : ""
+            }`}
+            onClick={() => {
+              setClickedZone(3);
+            }}
+          >
+            <ToolSvg svg={icon} />
+          </div>
+        ))}
+        {titleList.map((title, index) => (
+          <div className={`title-${index + 1}`}>
+            <p className="title-grid">{title}</p>
+          </div>
+        ))}
+        <div className="img-tool">
+          <ToolRefImg imgType={clickedZone} />
+        </div>
+        <div className="title-tool">
+          <h2>Tools.</h2>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/Tool/ToolRefImg.tsx
+++ src/components/Tool/ToolRefImg.tsx
@@ -4,47 +4,30 @@ import { Canvas } from "@react-three/fiber";
 import ToolAnimation from "../../assets/ToolAnimation.json";
 import Lottie from "lottie-react";
 
-const ToolDescp = () => {
-  const canvasSquareSize = 300;
-  const getToolImage = (type: string) => {
+interface Props {
+  imgType: number;
+}
+
+const ToolRefImg = ({ imgType }: Props) => {
+  const getToolImage = (type: number) => {
     switch (type) {
-      case "frontend":
+      case 1:
         return <p>front end Test</p>;
-      case "3D":
+      case 2:
         return (
           <Canvas style={{ width: "100%", height: "100%" }}>
             <OrbitControls />
             <ForestHouse scale={1.05} position={[0, -0.1, 0]} />
           </Canvas>
         );
-      case "2D":
+      case 3:
         return <Lottie animationData={ToolAnimation} />;
       default:
         return <p>default Test</p>;
     }
   };
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-end",
-        rowGap: "16px",
-      }}
-    >
-      <h2>Tools</h2>
-      <div
-        style={{
-          height: canvasSquareSize,
-          width: canvasSquareSize,
-          backgroundColor: "white",
-        }}
-      >
-        {getToolImage("2D")}
-      </div>
-    </div>
-  );
+  return <>{getToolImage(imgType)}</>;
 };
 
-export default ToolDescp;
+export default ToolRefImg;
