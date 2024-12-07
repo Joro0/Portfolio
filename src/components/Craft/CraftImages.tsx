7aec9c8d88028a1a45b93e8d1a426d81ab89df73
joro <jose.alonsozv12@gmail.com>
2024-12-07 14:24:18 -0500

    Enhaced LoadProblem on CraftSection

--- src/components/Craft/CraftImages.tsx
+++ src/components/Craft/CraftImages.tsx
@@ -1,15 +1,27 @@
+import { useEffect } from "react";
+
 interface Props {
   selectedItem: number;
   itemData: { title: string; text: string; img: string }[];
 }
 
 const CraftImages = ({ selectedItem, itemData }: Props) => {
+  useEffect(() => {
+    // Preload the image when the selected item changes
+    const imageToPreload1 = new Image();
+    const imageToPreload2 = new Image();
+    const imageToPreload3 = new Image();
+    imageToPreload1.src = itemData[0].img;
+    imageToPreload2.src = itemData[1].img;
+    imageToPreload3.src = itemData[2].img;
+  }, [selectedItem, itemData]);
+
   return (
     <div className="image-container">
       <img
         src={itemData[selectedItem].img}
-        loading="eager"
-        alt={`Craft image ${itemData[selectedItem].img + 1}`}
+        loading="lazy"
+        alt={`Craft image ${selectedItem + 1}`}
         key={selectedItem}
         className="image-item"
       />
