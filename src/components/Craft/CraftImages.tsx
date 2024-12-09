58b6f4098960c801b73a05f5d36b804c7a699c6e
joro <jose.alonsozv12@gmail.com>
2024-12-09 10:12:04 -0500

    Solving Image Craft: solved the blank delay

--- src/components/Craft/CraftImages.tsx
+++ src/components/Craft/CraftImages.tsx
@@ -14,16 +14,42 @@ const CraftImages = ({ selectedItem, itemData }: Props) => {
     imageToPreload1.src = itemData[0].img;
     imageToPreload2.src = itemData[1].img;
     imageToPreload3.src = itemData[2].img;
-  }, [selectedItem, itemData]);
+  }, []);
 
   return (
     <div className="image-container">
       <img
-        src={itemData[selectedItem].img}
+        src={itemData[0].img}
         loading="lazy"
-        alt={`Craft image ${selectedItem + 1}`}
-        key={selectedItem}
+        alt={`Craft image ${0 + 1}`}
+        key={0}
         className="image-item"
+        style={{
+          transform:
+            selectedItem === 0 ? "translate(0, 0)" : "translate(100%, 0)",
+        }}
+      />
+      <img
+        src={itemData[1].img}
+        loading="lazy"
+        alt={`Craft image ${1 + 1}`}
+        key={1}
+        className="image-item"
+        style={{
+          transform:
+            selectedItem === 1 ? "translate(-100%, 0)" : "translate(0, 0)",
+        }}
+      />
+      <img
+        src={itemData[2].img}
+        loading="lazy"
+        alt={`Craft image ${2 + 1}`}
+        key={2}
+        className="image-item"
+        style={{
+          transform:
+            selectedItem === 2 ? "translate(-200%, 0)" : "translate(0, 0)",
+        }}
       />
     </div>
   );
